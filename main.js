58461f79853f0e94505802bef28c2d876815cba9
Ona Boseroy <o.boseroy@gmail.com>
2015-11-06 14:45:35 -0500

    moved a bunch of files into cleaner directory. hopefully this works.

---
+++ main.js
@@ -0,0 +1,44 @@
+console.log("linked!");
+// $(document).ready(function() {
+
+
+// a  |  b  |  c  |  d  
+// e  |  f  |  g  |  h
+// i  |  j  |  k  |  l
+// m  |  n  |  o  |  p
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
