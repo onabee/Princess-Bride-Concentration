0e820f082d6e0b61b41f2b04c86f8f200c7f0a5a
Ona Boseroy <o.boseroy@gmail.com>
2015-11-06 18:49:43 -0500

    created vars for each square, created function for when user clicks square A

--- main.js
+++ main.js
@@ -1,43 +1,40 @@
 console.log("linked!");
-// $(document).ready(function() {
 
 
 // a  |  b  |  c  |  d  
 // e  |  f  |  g  |  h
 // i  |  j  |  k  |  l
 // m  |  n  |  o  |  p
-var $squares = $('.square')
 
-// var cards = [
-// 	[a, b, c, d],
-// 	[e, f, g, h],
-// 	[i, j, k, l],
-// 	[m, n, o, p],
-// ];
+//define each sqr as its own var
+var $squareA = $('#sqrA');
+var $squareB = $('#sqrB');
+var $squareC = $('#sqrC');
+var $squareD = $('#sqrD');
+var $squareE = $('#sqrE');
+var $squareF = $('#sqrF');
+var $squareG = $('#sqrG');
+var $squareH = $('#sqrH');
+var $squareI = $('#sqrI');
+var $squareJ = $('#sqrJ');
+var $squareK = $('#sqrK');
+var $squareL = $('#sqrL');
+var $squareM = $('#sqrM');
+var $squareN = $('#sqrN');
+var $squareO = $('#sqrO');
+var $squareP = $('#sqrP');
 
+// if a user clicks a card, it should reveal the back of the card
+var uncoverCard = function(event){
+  console.log(event);
 
+  if ($squareA.hasClass('back')) {
+      $squareA.removeClass('back');
+      $squareA.addClass('front');
+  } 
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$squareA.one('click', uncoverCard);
 
 
 
@@ -49,4 +46,16 @@ var $squares = $('.square')
 
 
 
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
